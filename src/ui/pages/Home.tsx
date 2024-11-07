9292646a31e0bec6cb4d4252af77ddd33c15a73d
Steven Lopez <stevenlopez009r@gmail.com>
2024-11-06 22:33:39 -0500

    adelanto de la ui home

--- src/ui/pages/Home.tsx
+++ src/ui/pages/Home.tsx
@@ -1,8 +1,118 @@
-import { useAuth } from "../../auth/AuthProvider";
+import { Box, TextField, Typography } from "@mui/material";
+import Carousel from "../components/Carousel";
+import FmdGoodIcon from "@mui/icons-material/FmdGood";
 
 function Home() {
-  const auth = useAuth();
-  return <h1>Home de {auth.getUser()?.name || ""}</h1>;
+  return (
+    <Box sx={{ width: "100%" }}>
+      <Box sx={{ margin: "0 auto", width: "90%" }}>
+        <Box
+          sx={{
+            height: "20vh",
+          }}
+        >
+          <Typography>Location</Typography>
+          <Box sx={{ display: "flex" }}>
+            <FmdGoodIcon sx={{ color: "#6e4a33" }} />
+            <Typography>Bogota, Colombia</Typography>
+          </Box>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              margin: "15px 0",
+            }}
+          >
+            <TextField
+              variant="outlined"
+              type="search"
+              placeholder="Search..."
+              sx={{
+                width: "85%",
+                backgroundColor: "#fff",
+                "& input": {
+                  padding: "6px 12px",
+                  fontSize: "14px",
+                },
+                "& .MuiOutlinedInput-root": {
+                  height: "50px",
+                },
+              }}
+            />
+            <Box
+              sx={{
+                height: "50px",
+                width: "50px",
+                backgroundColor: "#6e4a33",
+                borderRadius: "10px",
+              }}
+            ></Box>
+          </Box>
+        </Box>
+        <Carousel />
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "space-around",
+            margin: "20px 0",
+          }}
+        >
+          <Box>
+            <Box
+              sx={{
+                backgroundColor: "pink",
+                borderRadius: "50px",
+                width: "60px",
+                height: "60px",
+              }}
+            ></Box>
+            <Typography sx={{ textAlign: "center", fontSize: "0.8rem" }}>
+              SHIRT
+            </Typography>
+          </Box>
+          <Box>
+            <Box
+              sx={{
+                backgroundColor: "pink",
+                borderRadius: "50px",
+                width: "60px",
+                height: "60px",
+              }}
+            ></Box>
+            <Typography sx={{ textAlign: "center", fontSize: "0.8rem" }}>
+              PANT
+            </Typography>
+          </Box>
+          <Box>
+            <Box
+              sx={{
+                backgroundColor: "pink",
+                borderRadius: "50px",
+                width: "60px",
+                height: "60px",
+              }}
+            ></Box>
+            <Typography sx={{ textAlign: "center", fontSize: "0.8rem" }}>
+              DRESS
+            </Typography>
+          </Box>
+          <Box>
+            <Box
+              sx={{
+                backgroundColor: "pink",
+                borderRadius: "50px",
+                width: "60px",
+                height: "60px",
+              }}
+            ></Box>
+            <Typography sx={{ textAlign: "center", fontSize: "0.8rem" }}>
+              JACKET
+            </Typography>
+          </Box>
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 export default Home;
--- src/ui/pages/Splash.tsx
+++ src/ui/pages/Splash.tsx
@@ -1,15 +1,19 @@
-import { Link } from "react-router-dom";
-import { Button, Container, Grid } from "@mui/material";
+import { Box, Button, Typography, Grid } from "@mui/material";
 import img1 from "../assets/img1.jpg";
 import img2 from "../assets/img2.jpg";
 import img3 from "../assets/img3.jpg";
+import { Link, useNavigate } from "react-router-dom";
 
 function Splash() {
+  const navigate = useNavigate();
+  const handleButtonClick = () => {
+    navigate("/signup");
+  };
   return (
     <>
-      <div className="splash__container">
-        <Container>
-          <Grid container spacing={2}>
+      <Box sx={{ padding: "20px", height: "100vh" }}>
+        <Box sx={{ height: "70%" }}>
+          <Grid container spacing={1}>
             <Grid item xs={6}>
               <img
                 src={img3}
@@ -22,33 +26,74 @@ function Splash() {
                 }}
               />
             </Grid>
-            <Grid item xs={6} container direction="column" spacing={2}>
-              <Grid item>
+            <Grid item xs={6}>
+              <Box
+                sx={{
+                  display: "flex",
+                  flexDirection: "column",
+                  height: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
                 <img
                   src={img2}
                   alt=""
                   style={{
                     width: "100%",
-                    height: "auto",
+                    height: "48%",
+                    objectFit: "cover",
                     borderRadius: "90px",
                   }}
                 />
-              </Grid>
-              <Grid item>
                 <img
                   src={img1}
                   alt=""
                   style={{
                     width: "100%",
-                    height: "auto",
+                    height: "48%",
+                    objectFit: "cover",
                     borderRadius: "100px",
                   }}
                 />
-              </Grid>
+              </Box>
             </Grid>
           </Grid>
-        </Container>
-      </div>
+        </Box>
+        <Box
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            mt: 2,
+          }}
+        >
+          <Typography sx={{ fontSize: "1.5rem", textAlign: "center" }}>
+            The Fashion App That Makes You Look Your Best
+          </Typography>
+          <Typography sx={{ textAlign: "center", mt: 1 }}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo
+            beatae sed adipisci et
+          </Typography>
+          <Button
+            type="submit"
+            variant="contained"
+            sx={{
+              mt: 2,
+              py: 1.5,
+              backgroundColor: "#7f5539",
+              borderRadius: "20px",
+              "&:hover": {
+                backgroundColor: "#6e4a33",
+              },
+            }}
+            onClick={handleButtonClick}
+          >
+            Let's Get Started
+          </Button>
+          <Typography sx={{ textAlign: "center", mt: 1 }}>
+            Already have an account? <Link to={"/login"}>Sign In</Link>
+          </Typography>
+        </Box>
+      </Box>
     </>
   );
 }
